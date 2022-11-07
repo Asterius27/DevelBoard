510e3afdd8eaea4dcfa1da835a0bbef801171fc9
Coalessence <ettoreonecarb@gmail.com>
2022-11-07 17:15:40 +0100

    package update

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,13 +1,14 @@
 //https://neo4j.com/developer/js-movie-app/
 const express = require('express');
 import Neode from 'neode';
+const jwt = require('jsonwebtoken');
 
 //const instance = new Neode('bolt://localhost:7687', 'username', 'password');
 
 const app = express();
 
-const port = 3000;
+const port = 3000; //to change with env variable
 
 app.listen(port, () => {
-    console.log('app listening on port '+port)
-})
+    console.log('app listening on port '+port);
+});
