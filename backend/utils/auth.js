ff10c22c0190ea28463512b64e1ac222acdcf693
Asterius27 <878352@stud.unive.it>
2022-11-14 17:35:46 +0100

    fix

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -4,7 +4,7 @@ function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
   
-    if (token == null) return res.sendStatus(401).json({error: "Authentication failed"})
+    if (token == null) return res.sendStatus(401)
   
     jwt.verify(token, process.env.TOKEN, (err, data) => {
         console.log(err)
